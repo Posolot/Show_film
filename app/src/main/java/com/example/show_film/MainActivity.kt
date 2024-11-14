ca87fdf5194e212168167946cfc4873ddc617953
ilushenka <saharokbro@mail.ru>
2024-11-14 10:55:23 +0800

    first_send

---
+++ app/src/main/java/com/example/show_film/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.show_film
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val randomFilm: TextView = findViewById<TextView>(R.id.random_chosen_film)
+        val caution = findViewById<TextView>(R.id.caution)
+        val randomizeBtn = findViewById<Button>(R.id.randomize_btn)
+        val resetBtn = findViewById<Button>(R.id.reset_btn)
+        val films = resources.getStringArray(R.array.films)
+
+        val filmsCopy = mutableListOf<String>()
+
+        fun setRandomFilm() {
+            if (filmsCopy.isEmpty()) {
+                caution.visibility = View.VISIBLE
+                resetBtn.visibility = View.VISIBLE
+            } else {
+                val randomNum = (0..<filmsCopy.size).random()
+                randomFilm.text = filmsCopy[randomNum]
+                filmsCopy.removeAt(randomNum)
+            }
+        }
+
+        fun reset() {
+            for (i in films) {
+                filmsCopy.add(i)
+            }
+            caution.visibility = View.GONE
+            resetBtn.visibility = View.GONE
+            randomFilm.text = ""
+
+        }
+        reset()
+        randomizeBtn.setOnClickListener() {
+            setRandomFilm()
+        }
+
+        resetBtn.setOnClickListener() {
+            reset()
+        }
+    }
+
+}
